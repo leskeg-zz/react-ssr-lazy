7787e2069f2ec4f48e81096953560b452e01bb6c
Leske, Gabriel <gabriel_leske@epam.com>
2016-10-10 18:03:40 +0300

    Added 404 detection in server

--- src/server.js
+++ src/server.js
@@ -16,14 +16,22 @@ app.use(express.static(staticPath, {index: false}));
 // app.get('/test', (req, res) => res.send('test'));
 
 app.get('*', (req, res) => {
-    match({
-        routes,
-        location: req.url
-    }, (error, redirectLocation, renderProps) => {
-        const app = renderToString(<RouterContext {...renderProps}/>);
-        fs.readFile(path.join(staticPath, 'index.html'), 'utf8', (err, data = '') =>
-			res.send(data.replace('id="app">', `id="app">${app}`)) || err);
-    });
+	match({
+		routes,
+		location: req.url
+	}, (error, redirectLocation, renderProps) => {
+
+		// 404
+		if (renderProps === undefined) {
+			res.redirect('/');
+			return;
+		}
+
+		fs.readFile(path.join(staticPath, 'index.html'), 'utf8', (err, data = '') => {
+			const app = renderToString(<RouterContext {...renderProps}/>);
+			res.send(data.replace('id="app">', `id="app">${app}`)) || err;
+		});
+	});
 });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
